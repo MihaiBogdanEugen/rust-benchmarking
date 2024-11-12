86146570909ca382c61206a70584ab9f6b03c42c
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2024-11-12 21:58:48 +0100

    cli for passing args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use clap::Parser;
 use rust_benchmarking::isr::inverse_square_root::InverseSquareRoot;
 
+#[derive(Parser)]
+#[command()]
+struct Cli {
+    #[arg(short, long, value_parser = clap::value_parser!(f32))]
+    val: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
-    
-    let number: f32 = 12.24_f32;
+    let cli: Cli = Cli::parse();
+    let number: f32 = cli.val;
     let result: f32 = number.isr();
 
     println!("Inverse Squre Root of {number} is {result}")
